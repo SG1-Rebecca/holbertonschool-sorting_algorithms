0764144f013b7b1ed9fbcbc1560f8ae9370540ca
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-08 22:02:25 +0200

    fix betty coding style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,33 +3,29 @@
 * bubble_sort - sorts an array of integers in ascending order
 *@array: Array of integers
 *@size: Size of the arrays
-* 
-* Return: Nothing 
+*
+* Return: Nothing
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t round, index;
-    int tmp;
-
+size_t round, index;
+int tmp;
 /* if array is empty or size has less than 2 elements*/
-    if (array == NULL || size < 2)
-    {
-        return;
-    }
-    
-    for (round = 0; round < size - 1; round++)
-    {
-        for (index = 0; index < size - 1 - round; index++)
-        {
-            if (array[index] > array[index + 1])
-            {
-                tmp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = tmp;
-                print_array(array, size);
-            }
-            
-        }
-    }
-    
+if (array == NULL || size < 2)
+{
+return;
+}
+for (round = 0; round < size - 1; round++)
+{
+for (index = 0; index < size - 1 - round; index++)
+{
+if (array[index] > array[index + 1])
+{
+tmp = array[index];
+array[index] = array[index + 1];
+array[index + 1] = tmp;
+print_array(array, size);
+}
+}
+}
 }
